e9e070c26113862c2068a8815f08c5c7acc53ccd
mauro <mauro@gkz>
2018-04-18 00:40:43 -0300

    Se agregaron los comentarios del punto 4

--- src/Punto4/Arreglo.java
+++ src/Punto4/Arreglo.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Arreglo {
     public static void main (String args[]){
-        Scanner scan=new Scanner(System.in);
-        int B[]=new int[10];
-        System.out.println("Ingrese 10 numeros para el vector ");
-        for( int i=0;i<B.length;i++){
-            B[i]=scan.nextInt();
+        Scanner scan=new Scanner(System.in); // Se crea un objeto de tipo scanner 
+        int B[]=new int[10]; // Se define el arreglo 
+        System.out.println("Ingrese 10 numeros para el vector "); 
+        for( int i=0;i<B.length;i++){ 
+            B[i]=scan.nextInt(); // Guardamos los números ingresados en cada posición del vector
         }
-        mostrarVector(B);
-        
-        System.out.println("El numero mayor del vector es: "+mostrarMayor(B));
-        System.out.println("El numero menor del vector es: "+mostrarMenor(B));
-        System.out.println("El promedio es: "+ mostrarPromedio(B));
+        mostrarVector(B); // Imprimimos los datos del vector con el método mostrarVector
+         
+        System.out.println("El numero mayor del vector es: "+mostrarMayor(B)); // Usamos el método mostrarMayor 
+        System.out.println("El numero menor del vector es: "+mostrarMenor(B)); // Usamos el método mostrarMenor
+        System.out.println("El promedio es: "+ mostrarPromedio(B)); // Usamos el método mostrarPromedio
     }
-    public static void mostrarVector(int[] B){
+    public static void mostrarVector(int[] B){ // Creamos el método mostrarVector
         System.out.println("Los datos el vector son:");
-        for (int j=0;j<B.length;j++){
-            System.out.println(B[j]);
+        for (int j=0;j<B.length;j++){ 
+            System.out.println(B[j]); // Mostrará los datos del vector de cada posición
         }
     }
-    public static int mostrarMayor(int [] B){
+    public static int mostrarMayor(int [] B){ // Creamos el método mostrarMayor que comparará cada posición del vector y dará por resultado el mayor 
         int mayor=0;
         boolean band=true;
         for (int i=0;i<B.length;i++){
@@ -38,7 +38,7 @@ public class Arreglo {
         }
         return mayor;
     }
-    public static int mostrarMenor(int [] B){
+    public static int mostrarMenor(int [] B){ // Creamos el método mostrarMenor que comparará cada posición del vector y dará por resultado el menor
         int menor=0;
         boolean band=true;
         for (int i=0;i<B.length;i++){
@@ -54,7 +54,7 @@ public class Arreglo {
         }
         return menor;
     }
-    public static double mostrarPromedio(int [] B){
+    public static double mostrarPromedio(int [] B){ // Creamos el método mostrarPromedio, sumará todos los números de cada posición y lo dividirá por el número total de posiciónes
         int suma=0,cont=0;
         double promedio;
         
