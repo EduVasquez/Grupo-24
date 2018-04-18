50932d30c6aaa552ae4348ab6a7791449e59cc39
dhal_ <dhal_@LAPTOP-J53S6BN7>
2018-04-18 03:09:25 -0300

    se mejora el punto 4

--- src/Punto4/Arreglo.java
+++ src/Punto4/Arreglo.java
@@ -3,24 +3,24 @@ package Punto4;
 import java.util.Scanner;
 
 public class Arreglo {
-    public static void main (String args[]){
+   
+    public void mostrarVector(){ // Creamos el método mostrarVector
+        
         Scanner scan=new Scanner(System.in); // Se crea un objeto de tipo scanner 
-        int B[]=new int[10]; // Se define el arreglo 
+        int vector[]=new int[10]; // Se define el arreglo 
         System.out.println("Ingrese 10 numeros para el vector "); 
-        for( int i=0;i<B.length;i++){ 
-            B[i]=scan.nextInt(); // Guardamos los números ingresados en cada posición del vector
+        for( int i=0;i<vector.length;i++){ 
+            vector[i]=scan.nextInt(); // Guardamos los números ingresados en cada posición del vector
         }
-        mostrarVector(B); // Imprimimos los datos del vector con el método mostrarVector
-         
-        System.out.println("El numero mayor del vector es: "+mostrarMayor(B)); // Usamos el método mostrarMayor 
-        System.out.println("El numero menor del vector es: "+mostrarMenor(B)); // Usamos el método mostrarMenor
-        System.out.println("El promedio es: "+ mostrarPromedio(B)); // Usamos el método mostrarPromedio
-    }
-    public static void mostrarVector(int[] B){ // Creamos el método mostrarVector
+        
         System.out.println("Los datos el vector son:");
-        for (int j=0;j<B.length;j++){ 
-            System.out.println(B[j]); // Mostrará los datos del vector de cada posición
+        for (int j=0;j<vector.length;j++){ 
+            System.out.println(vector[j]); // Mostrará los datos del vector de cada posición
         }
+        
+        System.out.println("El numero mayor del vector es: "+mostrarMayor(vector)); // Usamos el método mostrarMayor 
+        System.out.println("El numero menor del vector es: "+mostrarMenor(vector)); // Usamos el método mostrarMenor
+        System.out.println("El promedio es: "+ mostrarPromedio(vector)); // Usamos el método mostrarPromedio
     }
     public static int mostrarMayor(int [] B){ // Creamos el método mostrarMayor que comparará cada posición del vector y dará por resultado el mayor 
         int mayor=0;
