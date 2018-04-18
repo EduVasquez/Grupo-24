4a4eda59065b6ce0839ee864b438a4dcc122bf4d
dhal_ <dhal_@LAPTOP-J53S6BN7>
2018-04-18 03:17:28 -0300

    agregado de cometario al punto 7

--- src/Punto7/ArregloDeAutos.java
+++ src/Punto7/ArregloDeAutos.java
@@ -12,35 +12,60 @@ package Punto7;
 public class ArregloDeAutos { // Craemos la clase ArregloDeAutos que será un Array
     private Auto[] arreglo;
     
+    /**
+     */
     public ArregloDeAutos() { // Definimos al constructor que será un Array con el tamaño
         arreglo = new Auto[5];
     }
-
+    
+    /**
+     * @param arreglo
+     */
     public ArregloDeAutos(Auto[] arreglo) {
         this.arreglo = arreglo;
     }
     
+    /**
+     * @param tamanioArreglo
+     */
     public ArregloDeAutos(int tamanioArreglo){  
         arreglo = new Auto[tamanioArreglo];
     }
     
-     public void agregarAuto(Auto auto, int posicion){
+    /**
+     * @param auto
+     * @param posicion
+     */
+    public void agregarAuto(Auto auto, int posicion){
         arreglo[posicion] = auto;
     }
     
+    /**
+     * se llama al metodo mostrar
+     */
     public void mostrarArreglo(){
         for(Auto a:arreglo){
             a.mostrarDatos();
         }
     }
-
+    
+    /**
+     * @return el valor de arreglo
+     */
     public Auto[] getArreglo() {
         return arreglo;
     }
-
+    
+    /**
+     * @param arreglo
+     */
     public void setArreglo(Auto[] arreglo) {
         this.arreglo = arreglo;
     }
+    
+    /**
+     * @param aut
+     */
      public void buscarAuto(String aut){ // Definimos el método buscarAuto
         for (Auto a : arreglo) {
             if(aut == null ? a.getNombre() == null : aut.equals(a.getNombre()))
--- src/Punto7/Auto.java
+++ src/Punto7/Auto.java
@@ -21,6 +21,14 @@ public class Auto { // Creamos la clase auto y definimos las variables que vamos
         
     }
     
+    /**
+     * @param nombre
+     * @param patente
+     * @param modelo
+     * @param marca
+     * @param color
+     * @param tipoCombustible
+     */
     public Auto(String nombre, String patente, int modelo, String marca, String color, String tipoCombustible){ // Constructor 2
         this.nombre=nombre;
         this.patente=patente;
@@ -30,44 +38,93 @@ public class Auto { // Creamos la clase auto y definimos las variables que vamos
         this.tipoCombustible=tipoCombustible;
     }
     
+    /**
+     * metodo mostrarDatos
+     */
     public void mostrarDatos(){ // Método que mostrará todas las variables del auto
          System.out.println(getNombre() + "        " +getPatente() +"         " +getModelo() + "       " +getMarca() + "         " +getColor() + "         " +getTipoCombustible());;
     }
     
+    /**
+     * @return el valor de nombre
+     */
     public String getNombre(){
         return nombre;
     }
+    
+    /**
+     * @return el valor de patente
+     */
     public String getPatente(){
         return patente;
     }
+    
+    /**
+     * @return el valor de modelo
+     */
     public int getModelo(){
         return modelo;
     }
+    
+    /**
+     * @return el valor de marca
+     */
     public String getMarca(){
         return marca;
     }
+    
+    /**
+     * @return el valor de color
+     */
     public String getColor(){
         return color;
     }
+    
+    /**
+     * @return el valor de tipoCombustible
+     */
     public String getTipoCombustible(){
         return tipoCombustible;
     }
     
+    /**
+     * @param nombre
+     */
     public void setNombre(String nombre){
         this.nombre=nombre;
     }
+    
+    /**
+     * @param patente
+     */
     public void setPatente(String patente){
         this.patente=patente;
     }
+    
+    /**
+     * @param modelo
+     */
     public void setModelo(int modelo){
         this.modelo=modelo;
     }
+    
+    /**
+     * @param marca
+     */
     public void setMarca(String marca){
         this.marca=marca;
     }
+    
+    /**
+     * @param color
+     */
     public void setColor(String color){
         this.color=color;
     }
+    
+    /**
+     * @param tipoCombustible
+     */
     public void setTipoCombustible(String tipoCombustible){
         this.tipoCombustible=tipoCombustible;
     }
